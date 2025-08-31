406b1ef3021a6aaae1bf927b2980560f5450b9f5
Anup Chaurasiya <anupchaurasiya81@gmail.com>
2025-08-31 19:53:30 +0530

    vercel

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,17 +2,20 @@ export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
   const options = {
     httpOnly: true,
-    secure: false, // secure in prod
-    sameSite: "lax", // or 'none' for cross-site
-     expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+    secure: process.env.NODE_ENV === "production", // must be true for HTTPS in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 'none' for cross-site
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    user,
-    message,
-    token,
-  });
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      user,
+      message,
+      token,
+    });
 };
